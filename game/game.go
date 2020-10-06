162f87e2cdbb01b1eb0400c131ab445ba86f2487
av.korovaev <av.korovaev@tensor.ru>
2020-10-06 18:14:54 +0300

    начал работать над наращиванием и движением змейки

--- game/game.go
+++ game/game.go
@@ -4,20 +4,36 @@ import (
 	tl "github.com/JoelOtter/termloop"
 )
 
-// Rungame основным объектом является игра
+// Rungame функция для инициализации основных объектов игры и её запуска
 func Rungame() {
 	tlgame := tl.NewGame()
 	tlgame.Screen().SetFps(30)
 
-	level := Getarea()
+	level := GetArea()
 
-	player := Player{
+	snake := Snake{
 		Entity: tl.NewEntity(1, 1, 1, 1),
 		level:  level,
 	}
+
+	snake1 := Snake{
+		Entity: tl.NewEntity(1, 2, 1, 1),
+		level:  level,
+	}
+
+	snake2 := Snake{
+		Entity: tl.NewEntity(1, 3, 1, 1),
+		level:  level,
+	}
+
 	// Set the character at position (0, 0) on the entity.
-	player.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
-	level.AddEntity(&player)
+	snake.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
+	snake1.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
+	snake2.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
+
+	level.AddEntity(&snake)
+	level.AddEntity(&snake1)
+	level.AddEntity(&snake2)
 	// установим созданный уровень на экран
 	tlgame.Screen().SetLevel(level)
 	// запустим игру
