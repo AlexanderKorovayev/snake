b03bc7287e184379454eeb2f64b25eebba497c94
av.korovaev <av.korovaev@tensor.ru>
2020-10-07 17:43:30 +0300

    изучаю библиотеку для работу с консолью

--- game/game.go
+++ game/game.go
@@ -16,24 +16,25 @@ func Rungame() {
 		level:  level,
 	}
 
-	snake1 := Snake{
-		Entity: tl.NewEntity(1, 2, 1, 1),
-		level:  level,
-	}
+	/*
+		snake1 := Snake{
+			Entity: tl.NewEntity(1, 2, 1, 1),
+			level:  level,
+		}
 
-	snake2 := Snake{
-		Entity: tl.NewEntity(1, 3, 1, 1),
-		level:  level,
-	}
-
+		snake2 := Snake{
+			Entity: tl.NewEntity(1, 3, 1, 1),
+			level:  level,
+		}
+	*/
 	// Set the character at position (0, 0) on the entity.
-	snake.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
-	snake1.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
-	snake2.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
+	snake.SetCell(0, 0, &tl.Cell{Fg: tl.ColorBlack, Ch: '█'})
+	//snake1.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
+	//snake2.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
 
 	level.AddEntity(&snake)
-	level.AddEntity(&snake1)
-	level.AddEntity(&snake2)
+	//level.AddEntity(&snake1)
+	//level.AddEntity(&snake2)
 	// установим созданный уровень на экран
 	tlgame.Screen().SetLevel(level)
 	// запустим игру
--- run.go
+++ run.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"github.com/AlexanderKorovayev/snakegame/game"
+	"fmt"
+	"reflect"
 )
 
+type Test [][]int
+
 func main() {
-	game.Rungame()
+	test := [][]int{{1, 1}, {2, 3}}
+	fmt.Println(test[1][1])
+	fmt.Println(reflect.TypeOf(test))
+	//game.Rungame()
 }
+
+/*
+есть canvas(полотно) это двумерный массив, который содержит Cell(ячейки),
+они могут содержать разные символы и цвета, таким образом рисуется канвас
+*/
+
+/*
+основные модули это:
+	entity.go
+	input.go
+	level.go
+	screen.go
+	termloop.go
+*/
