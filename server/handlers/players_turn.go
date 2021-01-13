9b4536e297df5f62331346538abcf4d342a0c43f
av.korovaev <av.korovaev@tensor.ru>
2021-01-13 10:27:06 +0300

    добавил значения по умолчанию для статусов и инфо

--- server/handlers/players_turn.go
+++ server/handlers/players_turn.go
@@ -56,6 +56,7 @@ func PlayersTurn(w http.ResponseWriter, r *http.Request) {
 	}
 	// обратно посылаем статусы, поэтому клиент будет ожидать строку
 	data.Action = ""
+	data.Info = ""
 	// определим, было ли столкновение змейки с самой собой
 	if snakeSelfCollision(core.MainObjects[clName]) {
 		// отправим статус о коллисии
